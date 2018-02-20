8e5b8a7b6e2976074c598173099d30a70d187c56
Vladislav Shishov <vshov@hotmail.com>
2018-02-20 20:14:45 +0300

    expression evaluator impl

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,13 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution.
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+    ) ["x"; "a"; "y"; "z"; "t"; "b"] *)
 
 (* Expression evaluator
 
@@ -50,5 +50,27 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval st e = 
+    match e with
+    | Const c -> c
+    | Var x -> st x
+    | Binop (op, lop, rop) -> 
+        let toBool i = i != 0 in
+        let toInt  b = if b then 1 else 0 in
+        let l = eval st lop in
+        let r = eval st rop in
+        match op with
+        | "+"  -> l + r 
+        | "-"  -> l - r 
+        | "*"  -> l * r 
+        | "/"  -> l / r 
+        | "%"  -> l mod r 
+        | "<"  -> toInt (l < r) 
+        | "<=" -> toInt (l <= r) 
+        | ">"  -> toInt (l > r) 
+        | ">=" -> toInt (l >= r) 
+        | "==" -> toInt (l == r) 
+        | "!=" -> toInt (l != r) 
+        | "&&" -> toInt (toBool l && toBool r) 
+        | "!!" -> toInt (toBool l || toBool r) 
+        | _    -> failwith "unknown operator"
