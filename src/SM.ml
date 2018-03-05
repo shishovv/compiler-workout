355975b59f253f5c8635065ce7604cbce917285c
Vladislav Shishov <vshov@hotmail.com>
2018-03-06 01:20:40 +0300

    hw2

--- src/SM.ml
+++ src/SM.ml
@@ -23,8 +23,27 @@ type config = int list * Syntax.Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
-
+let rec eval cfg prg =
+    match prg with
+    | [] -> cfg
+    | instr::instrs ->
+            let (stack, stmtCfg) = cfg in
+            let (st, input, output) = stmtCfg in
+            match instr with
+            | BINOP op -> 
+                    let l::r::s = stack in
+                    eval ((Syntax.Expr.evalBinOp op l r)::s,stmtCfg) instrs
+            | CONST c -> eval (c::stack, stmtCfg) instrs
+            | READ -> 
+                    let x::xs = input in
+                    eval (x::stack, (st, xs, output)) instrs
+            | WRITE -> 
+                    let x::xs = stack in
+                    eval (xs, (st, input, output@[x])) instrs
+            | LD var -> eval ((st var)::stack, stmtCfg) instrs
+            | ST var -> 
+                    let x::xs = stack in
+                    eval (xs, (Syntax.Expr.update var x st, input, output)) instrs
 (* Top-level evaluation
 
      val run : int list -> prg -> int list
@@ -41,4 +60,15 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+let rec compileExpr e =
+    match e with
+    | Syntax.Expr.Const c -> [CONST c]
+    | Syntax.Expr.Var var -> [LD var]
+    | Syntax.Expr.Binop (op, l, r) -> (compileExpr l)@(compileExpr r)@[BINOP op]
+
+let rec compile expr =
+    match expr with
+    | Syntax.Stmt.Read var -> [READ; ST var]
+    | Syntax.Stmt.Write e -> (compileExpr e)@[WRITE]
+    | Syntax.Stmt.Assign (var, e) -> (compileExpr e)@[ST var]
+    | Syntax.Stmt.Seq (e1, e2) -> (compile e1)@(compile e2)
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -2,14 +2,7 @@
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
 open GT 
-    
-(* Simple expressions: syntax and semantics *)
-module Expr =
-  struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
-    *)
+    (* Simple expressions: syntax and semantics *) module Expr = struct (* The type for expressions. Note, in regular OCaml there is no "@type..." notation, it came from GT.  *)
     @type t =
     (* integer constant *) | Const of int
     (* variable         *) | Var   of string
@@ -41,7 +34,33 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let evalBinOp op l r = 
+        let toBool i = i != 0 in
+        let toInt  b = if b then 1 else 0 in
+        match op with 
+        | "+"  -> l + r
+        | "-"  -> l - r
+        | "*"  -> l * r
+        | "/"  -> l / r
+        | "%"  -> l mod r
+        | "<"  -> toInt (l < r)
+        | "<=" -> toInt (l <= r)
+        | ">"  -> toInt (l > r)
+        | ">=" -> toInt (l >= r)
+        | "==" -> toInt (l == r)
+        | "!=" -> toInt (l != r)
+        | "&&" -> toInt (toBool l && toBool r)
+        | "!!" -> toInt (toBool l || toBool r)
+        | _    -> failwith "unknown operator"
+
+    let rec eval st e =
+    match e with
+    | Const c -> c
+    | Var x -> st x
+    | Binop (op, lop, rop) ->
+        let l = eval st lop in
+        let r = eval st rop in
+        evalBinOp op l r
 
   end
                     
@@ -65,7 +84,15 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval cfg p = 
+        let (st, input, output) = cfg in
+        match p with
+        | Read var -> 
+                let x::xs = input in
+                (Expr.update var x st, xs, output)
+        | Write e -> (st, input, output@[(Expr.eval st e)])
+        | Assign (var, e) -> (Expr.update var (Expr.eval st e) st, input, output)
+        | Seq (e1, e2) -> eval (eval cfg e1) e2 
                                                          
   end
 
